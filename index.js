49cf798c6e925ded4520504c285243d2626ff553
Yel <yelbenitez5@gmail.com>
2018-11-28 14:47:25 -0800

    parameters

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try{
   
     server.post('/getMovies',function (request,response)  { 
 
-        if(request.body.result.queryResult['top-rated']) {
+        if(request.body.result.parameters['top-rated']) {
             var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
                 req.query({
                     "page": "1",
