df2a2ad6a87caf22974c645ec91e61d1e5becbec
Yel <yelbenitez5@gmail.com>
2018-11-29 10:41:57 -0800

    switch statement

--- index.js
+++ index.js
@@ -20,51 +20,49 @@ try{
       
       var paramet = request.body;
      
-
-      if( paramet.queryResult.parameters["top-rated"] == 'top-rated') {
-      //  if(request.body.result.parameters['top-rated']){
-            var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-                req.query({
-                    "page": "1",
-                    "language": "en-US",
-                    "api_key": "736b1708b6070979b841db357f52d2cb"
-                });
-                req.send("{}");
-                req.end(function(res) {
-                    if(res.error) {
-                        response.setHeader('Content-Type', 'application/json');
-                        var pass = {
-                                    speech:'Error. Can you try it again',
-                                    displayText:'Error. Can you try it again ? '
-                                  }            
-                        response.send(pass);
-                    } else if(res.body.results.length > 0) {
-                        let result = res.body.results;
-                        let output = '';
-                        let nextline = ', ';
-                        for(let i = 0; i<result.length;i++) {
-                            output += result[i].title+nextline;
-                        } response.setHeader('Content-Type', 'application/json');
-                        
-                          var pass = {
-                            fulfillmentText : output
-                          }   
-                          response.send(pass); 
+          switch(paramet.intent["displayName"]){
+              case 'top movies':
+                    if(paramet.queryResult.parameters["top-rated"] == 'top-rated') {
+                      var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+                          req.query({
+                              "page": "1",
+                              "language": "en-US",
+                              "api_key": "736b1708b6070979b841db357f52d2cb"
+                          });
+                          req.send("{}");
+                          req.end(function(res) {
+                              if(res.error) {
+                                  response.setHeader('Content-Type', 'application/json');
+                                  var pass = {
+                                              speech:'Error. Can you try it again',
+                                              displayText:'Error. Can you try it again ? '
+                                            }            
+                                  response.send(pass);
+                              } else if(res.body.results.length > 0) {
+                                  let result = res.body.results;
+                                  let output = '';
+                                  let nextline = ', ';
+                                  for(let i = 0; i<result.length;i++) {
+                                      output += result[i].title+nextline;
+                                  } response.setHeader('Content-Type', 'application/json');
+                                  
+                                    var pass = {
+                                      fulfillmentText : output
+                                    }   
+                                    response.send(pass); 
+                              }
+                          });
                     }
-                });
-          }else{
-            response.setHeader('Content-Type', 'application/json');
-                var pass = {
-                             fulfillmentText  :'Error. Can you try it again'
-                          }            
-                response.send(pass);
+              break;
           }
-    });
-
+      });
 
   server.get('/getName',function (req,res){
       res.send('Swarup Bam');
   });
+
+
+
   }catch(err){
       console.log(err);
   }
